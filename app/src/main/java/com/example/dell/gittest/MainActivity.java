fb26e786473adc31861956413fe8a16223da5e56
56160017 <se56160017@gmail.com>
2016-12-05 23:27:55 +0700

    ejjj

--- app/src/main/java/com/example/dell/gittest/MainActivity.java
+++ app/src/main/java/com/example/dell/gittest/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("eiei", "onCreate: ");
+        Log.d("eiei", "onCreate: 2");
 
         Toast.makeText(getApplicationContext(), "eiei", Toast.LENGTH_SHORT).show();
     }

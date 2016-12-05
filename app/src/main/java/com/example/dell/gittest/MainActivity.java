40d4ca6c77f1f2c46aaca493ee005d03171d11a6
56160017 <se56160017@gmail.com>
2016-12-05 23:02:06 +0700

    eiei22

--- app/src/main/java/com/example/dell/gittest/MainActivity.java
+++ app/src/main/java/com/example/dell/gittest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dell.gittest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("eiei", "onCreate: ");
+        //Log.d("eiei", "onCreate: ");
+        Toast.makeText(getApplicationContext(), "eiei", Toast.LENGTH_SHORT).show();
     }
 }
